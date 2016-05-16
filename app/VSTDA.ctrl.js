ac87f693ff2c5db86d147d873c10d0aeddd2857a
Ji Min (Gloria) Jun <jiminisme@gmail.com>
2016-05-16 13:06:03 -0700

    added font

--- app/VSTDA.ctrl.js
+++ app/VSTDA.ctrl.js
@@ -12,14 +12,16 @@
 	        vm.title = 'VSTDACtrl';	        
 
 	        ////////////////
+//get ToDoData. called on activate
 	        var getToDoData = function() {
 	        	VSTDAFactory.getToDoData().then(function (response) {
 	        		vm.toDoList = response.data;
 	        	});
 	        };
 
+//add new ToDo. throws error if already exists
 	        vm.addToDo = function(newToDo) {
-	        	if (!newToDo.priority || !newToDo.description ) {
+	        	if (!newToDo.description ) {
 	        		alert("Please provide a description and priority of the ToDo item you want to add.");
 	        		return;
 	        	}
@@ -37,6 +39,7 @@
 	        	vm.newToDo = {};
 	        };
 
+//delete todo
 	        vm.deleteToDo = function(todo, index) {
 	        	VSTDAFactory.deleteToDoData(todo.vstdaEntryId)
 	        	.then(function (response) {
@@ -44,10 +47,11 @@
 	        	});
 	        };
 
+//updated todo
 	        vm.saveToDo = function (todo) {
-	        	if (!todo.updatedPriority) {
+	        	/*if (!todo.updatedPriority) {
 	        		todo.updatedPriority = todo.priority;
-	        	}
+	        	}*/
 
 	        	if (!todo.updatedTodo) {
 	        		todo.updatedDescription = todo.description;
@@ -72,10 +76,16 @@
 	        		});
 	        	});
 	        }
+
+	        vm.completedItem = function (todo) {
+	        	todo.oldPriority = todo.priority;
+	        	todo.updatedPriority = 9;
+	        	vm.saveToDo(todo);
+	        }
 //order items
-	vm.sortToDo = function (order) {
-        vm.toDoList = $filter('orderBy')(vm.toDoList, order);
-    };
+			vm.sortToDo = function (order) {
+		        vm.toDoList = $filter('orderBy')(vm.toDoList, order);
+		    };
 
 	        
 
